2b7655bfa893863c8f3a3fdf4ee0069f13c10461
perripez <pezzadkins@gmail.com>
2024-07-07 17:26:50 +1000

    Create grocery_operations package and display_groceries function

---
+++ grocery_store/src/grocery_operations_package/display_groceries.py
@@ -0,0 +1,11 @@
+def display_groceries(groceries):
+    """
+    Display list of groceries from JSON file
+    """
+    try:
+        for item in groceries:
+            print(f"{item["item"]} | ${item["price"]} ")
+    except KeyError as e:
+        print(f"Uh oh... error displaying groceries: missing key {e}")
+    except Exception as e:
+        print(f"Sorry! An unexpected error has occured: {e}")
